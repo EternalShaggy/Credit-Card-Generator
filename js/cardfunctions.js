b826d876ef19a1c2598f05373fb07bdb5cd11b07
EternalShaggy <121045783+Adey-Digital@users.noreply.github.com>
2023-01-23 18:56:23 -0500

    .
    .

--- js/cardfunctions.js
+++ js/cardfunctions.js
@@ -1,17 +1,4 @@
-//The following functions generatoes an array containing a random credit card number, a random cvc code, and a random date. 
-function generateCardDetails() {
-    // Credit card number
-    const cardNumber = Math.floor(100000000000000 + Math.random() * 900000000000000);
-    
-    // CVC code
-    const cvc = Math.floor(100 + Math.random() * 900);
-  
-    // Expiration date
-    const month = Math.floor(1 + Math.random() * 12);
-    const year = Math.floor(2022 + Math.random() * 10);
-  
-    displayCardInformation(cardNumber, cvc, `${month}/${year}`);
-  }
+
 
 //The following function copys the contents of the element card number to the devices clipboard
   function copyToClipboard() {
@@ -20,6 +7,27 @@ function generateCardDetails() {
     alert("Card number copied to clipboard!");
   }
 
-  function displayCardInformation(cardNumber, cvcNumber, expirationDate){
-    navigator
-  }
+  
+
+  //The following functions generatoes an array containing a random credit card number, a random cvc code, and a random date. 
+  function generateCardDetails() {
+    // Credit card number
+    let cardNumber = Math.floor(1000000000000000 + Math.random() * 9000000000000000);
+    
+    // CVC code
+    const cvc = Math.floor(100 + Math.random() * 900);
+  
+    // Expiration date
+    const month = Math.floor(1 + Math.random() * 12);
+    const year = Math.floor(2022 + Math.random() * 10);
+    // Add space every fourth digit
+    cardNumber = cardNumber.toString().replace(/(\d{4})/g, '$1 ');
+    displayCardInformation(cardNumber, cvc, `${month}/${year}`);
+}
+
+
+function displayCardInformation(cardNumber, cvcNumber, expirationDate){
+  document.getElementById("card_number").innerText = cardNumber;
+  document.getElementById("cvc_number").innerText = cvcNumber;
+  document.getElementById("expiration_date").innerText = expirationDate;
+}
